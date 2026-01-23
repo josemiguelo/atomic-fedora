c646fefadaa1be88658934c0186200665f729164
Jose Miguel Ochoa <josemiguelo.ochoa@gmail.com>
2026-01-23 13:35:42 -0500

    remove steam-fixer service

--- files/post-install-checker/post-install-checker-steam.sh
+++ files/post-install-checker/post-install-checker-steam.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-# steam
+# fix steam.desktop
 STEAM_AUTOSTART="$HOME/.config/autostart/steam.desktop"
 if [ -f "$STEAM_AUTOSTART" ]; then
   echo "Fixing steam autostart..."
@@ -11,3 +11,12 @@ if [ -f "$STEAM_AUTOSTART" ]; then
 else
   echo "Steam autostart file not found at $STEAM_AUTOSTART, skipping."
 fi
+
+# fix shaders background processing threads
+mkdir -p "$HOME/.steam/steam"
+cat <<EOF >"$HOME/.steam/steam/steam_dev.cfg"
+@ShaderBackgroundProcessingThreads 8
+unShaderBackgroundProcessingThreads 8
+EOF
+
+echo "Done fixing steam settings file"

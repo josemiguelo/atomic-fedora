5de30a8ff675073a2b279fcdb0ce453862e1d9d8
Jose Miguel Ochoa <josemiguelo.ochoa@gmail.com>
2026-02-08 22:11:48 -0500

    go back to main bluefin image

--- files/scripts/install-cosmic-desktop.sh
+++ files/scripts/install-cosmic-desktop.sh
@@ -26,35 +26,6 @@ echo "::group:: Install Additional Utilities"
 dnf5 install -y \
   xdg-desktop-portal-cosmic
 
-dnf5 remove -y \
-  NetworkManager-openconnect-gnome \
-  NetworkManager-openvpn-gnome \
-  NetworkManager-ssh-gnome \
-  NetworkManager-vpnc-gnome \
-  desktop-backgrounds-gnome \
-  f43-backgrounds-gnome \
-  gnome-abrt \
-  gnome-app-list \
-  gnome-autoar \
-  gnome-backgrounds \
-  gnome-calculator \
-  gnome-color-manager \
-  gnome-control-center \
-  gnome-control-center-filesystem \
-  gnome-disk-utility \
-  gnome-epub-thumbnailer \
-  gnome-initial-setup \
-  gnome-online-accounts \
-  gnome-online-accounts-libs \
-  gnome-settings-daemon \
-  gnome-shell-extension* \
-  nautilus \
-  gnome-tour \
-  gnome-user-docs \
-  gnome-user-share \
-  pinentry-gnome3 \
-  vlc-plugin-gnome
-
 echo "Additional utilities installed"
 echo "::endgroup::"
 
